e48106a44d931ab63e516771bc8e0acd8348e011
ZhangDubhe <zht_dubhe@163.com>
2018-05-22 00:59:25 +0800

    更新数据至2017年

--- TyphoonApi/convertData.py
+++ TyphoonApi/convertData.py
@@ -17,15 +17,19 @@ global HEADERS
 HEADERS = ''
 
 def readfileToPath(_file_, y):
+    """
+    读取最佳路径,并整理为点数据,转化为 csv 列表及导入数据库
+    """
     global HEADERS
     text = ''
-    replaceList = ["198415","198211","198801","199033","199120","199127","199216","200206","200215","200215","200126","200307","200226","200314","200401","200413","200509","200519","200601","200601","200604", "200608","200615","200615","200621","200908","200916","200917","201010"]
-    i = 0
-    info_row = []
     strong_level = ''
+    replaceList = ["198415","198211","198801","199033","199120","199127","199216","200206","200215","200215","200126","200307","200226","200314","200401","200413","200509","200519","200601","200601","200604", "200608","200615","200615","200621","200908","200916","200917","201010"]
+    info_row = []
+    OneList = []
     rowNum = 0
     j = 0
-    OneList = []
+    i = 0
+
     try:
         typhoonSelected = Typhoon.objects.get(num=str(y) + '01')
     except:
@@ -48,6 +52,7 @@ def readfileToPath(_file_, y):
             j = 0
             englishname = engName.title()
             engName = re.sub(' +', '', englishname)
+            # 先添加Typhoon点之后,再进行,可避免未查找到的情况
             try:
                 typhoonSelected = Typhoon.objects.get(num=typhoonId)
             except:
@@ -107,7 +112,6 @@ def readfileToPath(_file_, y):
         info_row = [str(typhoonId), date, str(strong_level), engName.rstrip(), str(j),  str(
             LAT), str(LON), Pressure, WND, OWD, str(replaceName), str(transit_extra_tropical)]
         print(info_row)
-        # info_row = [str(typhoonId),date, str(I)]
         nextTyphoon = Point(typhoonnumber=typhoonSelected, intensity=strong_level,
                             is_change=transit_extra_tropical, typhoontime=YYYYMMDDHH, 
                             happenedat=date, latitude=float(LAT), longitude=float(LON), windspeed=float(WND),
@@ -125,37 +129,41 @@ def readfileToPath(_file_, y):
 
 
 def readfileToGraph(_file_, y):
+    """
+    读取最佳路径,并整理为图表数据,转化为 csv 列表及导入数据库
+    """
     global HEADERS
     text = ''
-    replaceList = ["198415","198211","198801","199033","199120","199127","199216","200206","200215","200215","200126","200307","200226","200314","200401","200413","200509","200519","200601","200601","200604", "200608","200615","200615","200621","200908","200916","200917","201010"]
-    i = 0
-    info_row = []
     strong_level = ''
+    replaceList = ["198415","198211","198801","199033","199120","199127","199216","200206","200215","200215","200126","200307","200226","200314","200401","200413","200509","200519","200601","200601","200604", "200608","200615","200615","200621","200908","200916","200917","201010"]
+    OneList = []
+    info_row = []
     rowNum = 0
     j = 0
-    OneList = []
+    i = 0
     try:
         typhoonSelected = Typhoon.objects.get(num=str(y) + '01')
     except:
         pass
     for each in open(_file_, 'r'):
+        # 先判断是否为 '头' 行
         if "6666" == each[0:4]:
             nId = each[6:10]  # 国际编号 年份最后加两位数编号
             rowNum = each[12:15]  # 行数
-            id = each[16:20]  # 包括热带低压在内的序号
-
-            cId = each[21:25]  # 我国的编号
+            id = each[16:20]  # 包括热带低压在内的序号 4位
+            cId = each[21:25]  # 我国的编号 4位
             endInfo = each[26]  # 0 表示消散, 1 表示移出西太台风委员会的责任海区, 2 表示合并, 3 表示准静止;
-            engName = each[30:50]
+            engName = each[30:50] # 英文名
             year = y
-            typhoonId = str(year) + id[2:4]
+            typhoonId = str(year) + id[2:4] # 热带气旋编号,年份四位加包括热带低压编号后两位
             header_row = [year, nId, id, cId, endInfo, engName]
             print(header_row)
-            # HEADERS += ",".join(header_row) + '\r'
             strong_level = ''
             j = 0
+            # 处理英文名称多余空格
             englishname = engName.title()
             engName = re.sub(' +', '', englishname)
+            # 新增或查询已有 Typhoon 数据, 
             try:
                 typhoonSelected = Typhoon.objects.get(num=typhoonId)
             except:
@@ -166,6 +174,7 @@ def readfileToGraph(_file_, y):
                 print("*"*20, "NEW")
             print("Typhoon: ", typhoonSelected)
             continue
+        # 整理信息\字段
         YYYYMMDDHH = each[0:10]
         I = each[11]
         rowNum = int(rowNum)
@@ -181,8 +190,10 @@ def readfileToGraph(_file_, y):
             replaceName = True
         else:
             replaceName = False
+        # 补充初始化单个台风数据 Typhoon 未正确的信息
         if j == 0:
             typhoonSelected.startat = date
+            typhoonSelected.is_delate = replaceName
             typhoonSelected.save()
         elif j == rowNum-1:
             typhoonSelected.endat = date
@@ -191,40 +202,42 @@ def readfileToGraph(_file_, y):
         j += 1
         # 处理强度
         if int(I) == strong_level:
+            # 去掉重复的数据
             if j == rowNum:
+                # 末尾封闭
                 strong_level = 0
             else:
                 continue
         else:
             if j == rowNum:
+                # 末尾封闭
                 strong_level = 0
                 transit_extra_tropical = False
             elif int(I) == 9:
+                # 变性情况
                 if transit_extra_tropical == False:
                     strong_level = strong_level
                     transit_extra_tropical = True
                 else:
                     continue
             else:
+                # 正常传递数据
                 transit_extra_tropical = False
                 strong_level = int(I)
-        print(each)
 
         info_row = [str(typhoonId), date, str(strong_level), engName.rstrip(), str(j),  str(
             LAT), str(LON), Pressure, WND, OWD, str(replaceName), str(transit_extra_tropical)]
-        # print info_row
-        # info_row = [str(typhoonId),date, str(I)]
-        # nextTyphoon = GraphPoint(typhoonnumber=typhoonSelected, intensity=strong_level, is_change=transit_extra_tropical, typhoontime=YYYYMMDDHH, happenedat=date)
-        # OneList.append(nextTyphoon)
-        typhoonSelected.is_delate = replaceName
-        typhoonSelected.save()
+        # 添加单个台风图表点数据.
+        nextTyphoon = GraphPoint(typhoonnumber=typhoonSelected, intensity=strong_level, is_change=transit_extra_tropical, typhoontime=YYYYMMDDHH, happenedat=date)
+        OneList.append(nextTyphoon)
+
         text += ','.join(info_row)
         text += '\r'
         i += 1
 
     # 处理列表之后导入数据库
     print("Length: ", len(OneList))
-    # GraphPoint.objects.bulk_create(OneList)
+    GraphPoint.objects.bulk_create(OneList)
     return text
 
 
@@ -242,10 +255,12 @@ def findall(dir):
             print(path, "finish~")
     store_all(text, "someData")
 
-def querySpecialYear(dir):
+def querySpecialYear(dir, query_type, startYear, endYear):
+    """
+    读取最佳路径, 按年份循环
+    """
     text = ''
-    for i in range(1950,2015):
-        year = i
+    for year in range(startYear, endYear):
         name = "CH" + str(year) + "BST.txt"
         path = dir + name
         text = "id,date,I,name,seq,LAT,LON,Pressure,WND,OWD,replaceName,IsChange\r"
@@ -253,13 +268,17 @@ def querySpecialYear(dir):
         last_row = str(year) + "99,"+ str(year) +"-12-31 23:59:59,0,First,0,0,0,0,0,,False,False"
         text += first_row
         # text = "id,date,I"
-        # text += readfileToPath(path, year)  # 从最佳数据集里读取数据
-        text += readfileToGraph(path, year)  # 从最佳数据集里读取数据
+        if query_type == 'path':
+            text += readfileToPath(path, year)  # 从最佳数据集里读取数据
+        else:
+            text += readfileToGraph(path, year)  # 从最佳数据集里读取数据
         text += last_row
-        #  store path
-        # store_all(text, "../dataprocess/exportData/chart/"+str(year)) # To graph
-        # store_all(text, "../dataprocess/exportData/path/"+str(year)) # To path
-        print("Finished. At ", time.asctime(time.localtime(time.time())) )
+        # store path
+        if query_type == 'graph':
+            store_all(text, "../dataprocess/exportData/chart/"+str(year)) # To graph
+        else:
+            store_all(text, "../dataprocess/exportData/path/"+str(year)) # To path
+        print("Finished <", year, "> At ", time.asctime(time.localtime(time.time())) )
 
 def store_all(_text_, _name_):
     txt_path = _name_ + ".csv"
@@ -267,7 +286,6 @@ def store_all(_text_, _name_):
     _file = open(path, "w")
     _file.write(_text_)
     _file.close()
-
 
 def readLandFile():
     text = ""
@@ -306,7 +324,10 @@ def readLandFile():
 
 def main():
     global HEADERS
-    querySpecialYear("../dataprocess/originalData/bst4915/")
+    querySpecialYear("../dataprocess/originalData/bst4915/",
+                     'graph', 1949, 2018)
+    querySpecialYear("../dataprocess/originalData/bst4915/",
+                     'path', 1949, 2018)
     # readLandFile()
     # store_all(HEADERS, "headerData")
 
