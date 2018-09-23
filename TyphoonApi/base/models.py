ecf92cf0175bd58285af3943c2dab531bf5832e2
ZhangDubhe <zht_dubhe@163.com>
2018-09-23 19:40:16 +0800

    新增 base app 用于保存公众号对应的用户及用户信息

---
+++ TyphoonApi/base/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class TimeStampedModel(models.Model):
+    """ TimeStampedModel
+    An abstract base class model that provides self-managed "created" and "modified" fields.
+    """
+    uuid = models.UUIDField(auto_created=True, default=uuid.uuid4, editable=False, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        get_latest_by = 'updated_at'
+        ordering = ('-created_at', '-updated_at',)
+        abstract = True
+
+
+class User(AbstractUser):
+    uuid = models.UUIDField(auto_created=True, default=uuid.uuid4, editable=False, db_index=True)
+    username = models.CharField(
+        'username',
+        max_length=32,
+        unique=True,
+        error_messages={
+            'unique': "A user with that username already exists.",
+        },
+    )
+    first_name = models.CharField('first name', max_length=30, null=True, blank=True)
+    last_name = models.CharField('last name', max_length=30, null=True, blank=True)
+    mina_openid = models.CharField(max_length=32, null=True, db_index=True)
+    mp_openid = models.CharField(max_length=32, null=True, db_index=True)
+    union_id = models.CharField(max_length=32, null=True, db_index=True)
+    nick_name = models.CharField(max_length=128, null=True)
+    phone_number = models.CharField(max_length=20, null=True, db_index=True)
+    state = models.PositiveSmallIntegerField(default=0)
+    child = models.ForeignKey('self', null=True, default=None)
+    avatar_url = models.URLField(null=True)
+    is_subscribed = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'auth_user'
+
+
+class UserInfo(TimeStampedModel):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, primary_key=True, related_name='info')
+    sex = models.PositiveSmallIntegerField(null=True)
+    city = models.CharField(max_length=20, null=True)
+    school = models.CharField(max_length=20, null=True)
+    grade = models.CharField(max_length=20, null=True)
+    purpose = models.CharField(max_length=20, null=True)
+    subscribe_at = models.PositiveIntegerField(null=True)
+    unsubscribe_at = models.PositiveIntegerField(null=True)
+    reading_type = models.CharField(max_length=100, null=True)
+    exam_result = models.CharField(max_length=255, null=True, default='[]')
+
+    class Meta:
+        db_table = 'user_info'
--- TyphoonApi/tools/urls.py
+++ TyphoonApi/tools/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from tools.wechat.mp import *
+from tools.wechat.mina import *
 from . import views
 
 urlpatterns = [
     path('qrcode/<path:url>', views.generate_qrcode, name='qrcode'),
-    path('qrcode/', views.init_qrcode, name='initqrcode')
+    path('qrcode/', views.init_qrcode, name='initqrcode'),
+    path('wechat/mp', views.init_qrcode, name='initqrcode'),
+    path('wechat/mina', views.init_qrcode, name='initqrcode')
 ]
